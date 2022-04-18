f99f0100ef634278284e0a0c4043ee79265d95ff
kicok <kicoker@gmail.com>
2022-04-18 18:19:45 +0900

    환경셋팅완료

---
+++ src/test/java/com/nanum/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanum.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
