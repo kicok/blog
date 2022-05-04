acaeae694baa4c6861ed4466427594ab811e15ac
kicok <kicoker@gmail.com>
2022-05-04 19:36:32 +0900

    static 폴더의 404 NotFound 해결을 위한 configuration

---
+++ src/main/java/com/nanum/blog/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.nanum.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
