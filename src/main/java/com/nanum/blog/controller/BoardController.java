c1d3232b5c008b8e117c29e31836de2f7484284b
kicok <kicoker@gmail.com>
2022-05-04 17:27:40 +0900

    view 화면 구성 및 controller 작성

---
+++ src/main/java/com/nanum/blog/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.nanum.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping({"", "/"})
+    public String index(){
+        return "index";
+    }
+}
--- src/main/java/com/nanum/blog/test/TempControllerTest.java
+++ src/main/java/com/nanum/blog/test/TempControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TempControllerTest {
 
-    @GetMapping("/")
+    @GetMapping("/temp")
     public String index(String name, Model model) {
         model.addAttribute("name", name);
         return "index";
