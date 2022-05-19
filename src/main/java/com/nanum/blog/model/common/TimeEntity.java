a0764fe329b036404586978c8b4cf8b31bee37ae
kicok <kicoker@gmail.com>
2022-05-19 17:20:43 +0900

    JsonFormat-배열형태의 YMD 를 String 형태로 변경

--- src/main/java/com/nanum/blog/model/common/TimeEntity.java
+++ src/main/java/com/nanum/blog/model/common/TimeEntity.java
@@ -1,5 +1,6 @@
 package com.nanum.blog.model.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -14,10 +15,13 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeEntity {
+
     @CreatedDate
     @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createDate;
 
     @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifyDate;
 }
